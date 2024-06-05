55886ad56a32841b53057f4fe1229efb0c6ac654
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-06-05 16:19:48 +0000

    first commit

---
+++ app/Background.js
@@ -0,0 +1,29 @@
+import "./Background.css";
+
+export default function Background() {
+  return (
+    <div className="blobs-container">
+      <div className="blob-cont">
+        <div className="yellow blob"></div>
+        <div className="red blob"></div>
+        <div className="green blob"></div>
+      </div>
+      <svg>
+        <filter id="noiseFilter">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="1"
+            stitchTiles="stitch"
+          />
+          <feColorMatrix
+            in="colorNoise"
+            type="matrix"
+            values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0"
+          />
+          <feComposite operator="in" in2="SourceGraphic" result="monoNoise" />
+          <feBlend in="SourceGraphic" in2="monoNoise" mode="screen" />
+        </filter>
+      </svg>
+    </div>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Background from "./Background";
+import "./page.css";
+
+export default function Page() {
+  return (
+    <>
+      <Background />
+      <h1>O site que você precisava</h1>
+    </>
+  );
+}
